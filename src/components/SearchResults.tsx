4a9b1afa043e52d15ec0a97c330d73146efc5e6f
Julio Machado <juliocarlos00@hotmail.com>
2021-05-17 16:27:06 -0300

    add/ useCallback example

--- src/components/SearchResults.tsx
+++ src/components/SearchResults.tsx
@@ -3,9 +3,10 @@ import { ProductItem } from "./ProductItem";
 
 interface SearchResultsProps {
     results: Array<{ id: number, price: number; title: number; }>
+    onAddToWishList: (id: number) => void;
 }
 
-export function SearchResults({ results }: SearchResultsProps) {
+export function SearchResults({ results, onAddToWishList }: SearchResultsProps) {
 
     const totalPrice = useMemo(() => {
         return results.reduce((total, product) => {
@@ -22,7 +23,7 @@ export function SearchResults({ results }: SearchResultsProps) {
 
             {results.map(product => {
                 return (
-                    <ProductItem key={product.id} product={product} />
+                    <ProductItem key={product.id} product={product} onAddToWishList={onAddToWishList} />
                 );
             })}
         </div>
